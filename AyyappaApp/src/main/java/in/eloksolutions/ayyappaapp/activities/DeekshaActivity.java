c74b2c873820e66eeb581d47c60f948d2cc9f994
eloksolution <eloksolution@gmail.com>
2017-12-02 14:35:24 +0530

    Deeksha Activity Changed
    Deeksha Activity Changed

--- AyyappaApp/src/main/java/in/eloksolutions/ayyappaapp/activities/DeekshaActivity.java
+++ AyyappaApp/src/main/java/in/eloksolutions/ayyappaapp/activities/DeekshaActivity.java
@@ -1,11 +1,11 @@
 package in.eloksolutions.ayyappaapp.activities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -32,9 +32,10 @@ import in.eloksolutions.ayyappaapp.config.Config;
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.shedule);
-        final TextView edate=(TextView) findViewById(R.id.txtdate);
+        ctx=this;
+         /*final TextView edate=(TextView) findViewById(R.id.txtdate);
         final TextView tdate=(TextView) findViewById(R.id.fdate);
-        final TextView desc=(TextView) findViewById(R.id.description);
+        final TextView desc=(TextView) findViewById(R.id.description);*/
       Toolbar  toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Start Deeksha");
@@ -43,27 +44,32 @@ import in.eloksolutions.ayyappaapp.config.Config;
         SharedPreferences preferences = getSharedPreferences(Config.APP_PREFERENCES, MODE_PRIVATE);
        final String memId=preferences.getString("userId", null);
         Log.i(Tag,"userid is memId"+memId);
-        tDate=tdate.getText().toString();
-        eDate=edate.getText().toString();
+
         fdate = (TextView) findViewById(R.id.fdate);
-            fdate.setText("" + DateFormat.format(" dd-MM-yyyy", System.currentTimeMillis()));
+          //  fdate.setText("" + DateFormat.format(" dd-MM-yyyy", System.currentTimeMillis()));
         txtdate = (TextView) findViewById(R.id.txtdate);
-           txtdate.setText("" + DateFormat.format(" dd-MM-yyyy", System.currentTimeMillis()));
+         //  txtdate.setText("" + DateFormat.format("dd-MM-yyyy", System.currentTimeMillis()));
         description = (EditText) findViewById(R.id.description);
 
         fdate.setOnClickListener(this);
         txtdate.setOnClickListener(this);
         description.setOnClickListener(this);
-
-
+        tDate=fdate.getText().toString();
+        eDate=txtdate.getText().toString();
+        Log.i(Tag,"userid is memId"+tDate);
+        Log.i(Tag,"eDate"+eDate);
 }
     public void deekshaShare(){
         SharedPreferences sharedPreferences = ctx.getSharedPreferences(Config.APP_PREFERENCES, ctx.MODE_PRIVATE);
         SharedPreferences.Editor edit = sharedPreferences.edit();
-        edit.putString("startDate", tDate);
-        edit.putString("endDate", eDate);
+        edit.putString("startDate", fdate.getText().toString());
+        edit.putString("endDate", txtdate.getText().toString());
+        Log.i(Tag,"deeksha eDate ::"+txtdate.getText().toString());
+        Log.i(Tag,"deeksha from ::"+fdate.getText().toString());
         edit.commit();
         Log.i(Tag,"Start date is"+sharedPreferences.getString("startDate",null));
+        Intent active=new Intent(DeekshaActivity.this,CardViewActivity.class);
+        startActivity(active);
 
     }
     public void onClick(View v) {
@@ -78,6 +84,7 @@ import in.eloksolutions.ayyappaapp.config.Config;
 
 
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
